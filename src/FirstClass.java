21e2eadb04a1b7ae436369f3b82cd8e63b53f2d2
LuisKuckel <meloluisguilherme@gmail.com>
2026-03-03 01:03:57 -0300

    Entendendo sobre classes básico

---
+++ src/FirstClass.java
@@ -0,0 +1,5 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.print("Tudo dentro de um parentese é um método que busca parametros para ser executado");
+    }
+}
